6f776e1256ea63d07d13888849722f0d8eca94a2
WillMaac <anderrwil@gmail.com>
2025-11-16 14:17:26 -0300

    Ajustes nos links e nos cards de projetos, experiência e certificados

--- src/app/components/Card/Card.tsx
+++ src/app/components/Card/Card.tsx
@@ -10,7 +10,7 @@ type Props = {
 
 export const Card: React.FC<Props> = ({ titulo, descricao, btn, downloadLink, onClick, link }) => {
   return (
-    <article className="border-2 h-[20em] w-[25em] rounded-xl shadow-xl shadow-[#00000065] px-6 py-3 transition text-center duration-300 hover:scale-105 max-w-[20em] border-none">
+    <article className="border-2 h-[20em]  rounded-xl shadow-xl shadow-[#00000065] px-6 py-3 transition text-center duration-300 hover:scale-105 max-w-[25em] border-none w-full">
       <h3 className="text-xl font-inter">{titulo}</h3>
       <p className="text-[20px] font-poppins text-2xl">{descricao}</p>
 
@@ -20,7 +20,7 @@ export const Card: React.FC<Props> = ({ titulo, descricao, btn, downloadLink, on
             <a 
               href={downloadLink} 
               download 
-              className="border-2 py-2 px-2 bg-black border-none text-white rounded-[8px] mt-5 cursor-pointer transition duration-300 ease-in-out hover:scale-105 hover:bg-[#48b948]"
+              className="border-2 py-2 px-2 bg-black border-none text-white rounded-[8px] mt-5 cursor-pointer transition duration-300 ease-in-out hover:scale-105 hover:bg-[#48b948] inline-block"
             >
               {btn}
             </a>
